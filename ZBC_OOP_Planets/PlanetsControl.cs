efdf467af91ff56854c112c35cc5f5de2b20c941
Carlo2 <zbccmvm@ZBC-E-RO-23246>
2021-09-06 10:57:58 +0200

    n

---
+++ ZBC_OOP_Planets/PlanetsControl.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZBC_OOP_Planets
+{
+    public class PlanetsControl
+    {
+        private PlanetsDatabase database;
+
+        public PlanetsControl()
+        {
+            database = new PlanetsDatabase();
+        }
+
+        public void InsertPlanet(int index, string name)
+        {
+            database.InsertPlanetAtIndex(index, name);
+        }
+
+        public void AddPlanet(string name)
+        {
+            database.AddPlanet(name);
+        }
+
+        public void ClearDatabase()
+        {
+            database.ClearDatabase();
+        }
+
+        public List<Planet> GetPlanetsOfSize(int min, int max)
+        {
+            List<Planet> PlanetsList = database.GetPlanetsList();
+
+            List<Planet> newList = PlanetsList.FindAll(planet => planet.Diameter > min && planet.Diameter < max);
+            return newList;
+        }
+
+        public List<Planet> GetFreezingPlanets()
+        {
+            List<Planet> PlanetsList = database.GetPlanetsList();
+            List<Planet> newList = PlanetsList.FindAll(planet => planet.MeanTemperature < 0);
+
+            return newList;
+        }
+
+        public int GetPlanetsCount()
+        {
+            return database.GetPlanetsCount();
+        }
+
+        public void RemovePlanet(string name)
+        {
+            List<Planet> planetsList = database.GetPlanetsList();
+
+            // Find the index where a planet called Pluto resides
+            int index = planetsList.FindIndex(planet => planet.Name == "Pluto");
+
+            // If it's in the list, remove it
+            if (index != -1)
+            {
+                database.RemovePlanetAtIndex(index);
+            }
+        }
+
+        public void FillStarterList()
+        {
+            database.FillStarterList();
+        }
+
+        public List<Planet> GetPlanetsList()
+        {
+            return database.GetPlanetsList();
+        }
+    }
+}
---
+++ ZBC_OOP_Planets/UI.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZBC_OOP_Planets
+{
+    public class UI
+    {
+        private PlanetsControl control;
+
+        public void MainProgram()
+        {
+            control = new PlanetsControl();
+
+            Console.WriteLine("Press any key to initialize list.");
+            Console.ReadKey();
+
+            control.FillStarterList();
+            Console.WriteLine("Initializing list...\n\r\n\r");
+
+
+            // Printing the list to console
+            PrintList(control.GetPlanetsList());
+
+            // Inserting Venus
+            AskToContinue();
+
+            Console.WriteLine("\n\rInserting Venus between Mercury and Earth\n\r");
+            control.InsertPlanet(1, "Venus");
+
+            // Printing the list to console
+            PrintList(control.GetPlanetsList());
+
+            AskToContinue();
+
+            Console.WriteLine("\n\rRemoving Pluto from list :( https://i.imgur.com/GyVbdMy.jpg");
+            control.RemovePlanet("Pluto");
+
+            // Printing the list to console
+            PrintList(control.GetPlanetsList());
+
+            AskToContinue();
+
+            Console.WriteLine("\n\rInserting Pluto again, yay!");
+
+            control.AddPlanet("Pluto");
+
+            // Printing the list to console
+            PrintList(control.GetPlanetsList());
+
+            Console.WriteLine($"\n\rList count: {control.GetPlanetsCount()}");
+
+            AskToContinue();
+
+            Console.WriteLine("\n\rCreating a new list with the planets whos mean temperature is lower than 0");
+           
+
+            PrintList(control.GetFreezingPlanets());
+
+            AskToContinue();
+
+            Console.WriteLine("\n\rCreating a new list with the planets whos diameter is higher than 10,000km and lower than 50,000km");
+           
+            PrintList(control.GetPlanetsOfSize(10000, 50000));
+
+            AskToContinue();
+
+            Console.WriteLine("\n\rNow removing ALL planets from the original list");
+
+            control.ClearDatabase();
+
+            Console.WriteLine("\n\rCurrent list contents...\n\r");
+            PrintList(control.GetPlanetsList());
+
+            Console.WriteLine("--- End of assignment");
+        }
+
+        private void AskToContinue()
+        {
+            Console.WriteLine("\n\rPress any key to go on.\n\r");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Prints the contents of a Planet list to console
+        /// </summary>
+        /// <param name="list"></param>
+        static void PrintList(List<Planet> list)
+        {
+            Console.WriteLine("\n\rList contents: \n\r");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"{i} - {list[i].Name}");
+            }
+        }
+    }
+}
